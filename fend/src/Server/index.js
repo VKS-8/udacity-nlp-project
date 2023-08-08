6acf8d377d8137868f08cb4fc74cfab069f9662d
VK Johnson <64655526+VKS-8@users.noreply.github.com>
2023-08-08 13:58:19 -0500

    Initial changes to set up api call

--- fend/src/Server/index.js
+++ fend/src/Server/index.js
@@ -1,10 +1,8 @@
 const path = require('path')
 const express = require('express')
-const mockAPIResponse = require('./mockAPI.js')
-// const dotenv = require('dotenv')
-// dotenv.config();
-
-// require('dotenv').config();
+const nlpAPIResponse = require('./nlpAPI.js')
+const dotenv = require('dotenv')
+dotenv.config();
 
 const app = express()
 
@@ -14,7 +12,6 @@ console.log(__dirname)
 
 app.get('/', function (req, res) {
     res.sendFile('dist/index.html')
-    // res.sendFile(path.resolve('src/client/views/index.html'))
 })
 
 // designates what port the app will listen to for incoming requests
@@ -23,5 +20,5 @@ app.listen(8080, function () {
 })
 
 app.get('/nlp', function (req, res) {
-    res.send(mockAPIResponse)
+    res.send(nlpAPIResponse)
 })
---
+++ fend/src/Server/nplAPI.js
@@ -0,0 +1,6 @@
+const BASE_URL = 'https://api.example.com'; // Replace with your API's base URL
+
+export const APIEndpoints = {
+    getData: `${BASE_URL}/data`,
+    // Add more endpoints here
+};

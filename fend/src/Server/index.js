ebfbfa0f3fd88790d987f488322e70a74b5693a9
VK Johnson <64655526+VKS-8@users.noreply.github.com>
2023-08-08 21:34:10 -0500

    Change port listening method

--- fend/src/Server/index.js
+++ fend/src/Server/index.js
@@ -15,10 +15,15 @@ app.get('/', function (req, res) {
 })
 
 // designates what port the app will listen to for incoming requests
-app.listen(8080, function () {
-    console.log('NLP project app listening on port 8080!')
-})
+// app.listen(8080, function () {
+//     console.log('NLP project app listening on port 8080!')
+// })
 
-app.get('/nlp', function (req, res) {
+app.get('/nlpAPI', function (req, res) {
     res.send(nlpAPIResponse)
 })
+
+// designates what port the app will listen to for incoming requests
+app.listen(port, "0.0.0.0", () => {
+  console.log(`Server running on http://localhost:${port}`);
+});

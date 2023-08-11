c763e6c6ecacc3a3543cb1e9d946a586a610f1b0
VK Johnson <64655526+VKS-8@users.noreply.github.com>
2023-08-11 12:04:56 -0500

    Code correction

--- fend/webpack.dev.mjs
+++ fend/webpack.dev.mjs
@@ -4,35 +4,36 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
-entry: './src/client/index.mjs',
-mode: 'development',
-devtool: 'source-map',
-stats: 'verbose',
-module: {
-  rules: [
-    {
-      test: /\.mjs$/,
-      exclude: /node_modules/,
-      loader: "babel-loader"
-    },
-    {
-      test: /.s[ac]ss$/,
-      use: ['style-loader', 'css-loader', 'sass-loader']
-    }
+  entry: './src/client/index.mjs',
+  mode: 'development',
+  devtool: 'source-map',
+  stats: 'verbose',
+  module: {
+    rules: [
+      {
+        test: /\.mjs$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      },
+      {
+        test: /.s[ac]ss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/client/views/index.html",
+      filename: "index.html"
+    }),
+    new CleanWebpackPlugin({
+      // Simulate the removal of files
+      dry: true,
+      // Write Logs to Console
+      verbose: true,
+      // Automatically remove all unused webpack assets on rebuild
+      cleanStaleWebpackAssets: true,
+      protectWebpackAssets: false
+    })
   ]
-},
-plugins: [
-  new HtmlWebpackPlugin({
-    template: "./src/client/views/index.html",
-    filename: "index.html"
-  }),
-  new CleanWebpackPlugin({
-    // Simulate the removal of files
-    dry: true,
-    // Write Logs to Console
-    verbose: true,
-    // Automatically remove all unused webpack assets on rebuild
-    cleanStaleWebpackAssets: true,
-    protectWebpackAssets: false
-  })
-]
+}

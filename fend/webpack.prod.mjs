41ab39421c825e01d7040fff739fc9d1c65285ba
VK Johnson <64655526+VKS-8@users.noreply.github.com>
2023-08-11 13:44:50 -0500

    Add output

--- fend/webpack.prod.mjs
+++ fend/webpack.prod.mjs
@@ -1,23 +1,56 @@
 const path = require('path')
 const webpack = require('webpack')
-const HtmlWebpackPlugin = require("html-webpack-plugin")
+const HtmlWebPackPlugin = require("html-webpack-plugin")
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
-  entry: './src/client/index.js',
-  mode: 'production',
-  module: {
-      rules: [
-          {
+    entry: './src/client/index.js',
+    mode: 'development',
+    resolve: {
+      extensions: ['.js']
+    },
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist'), // Output directory,
+      libraryTarget: 'var',
+      library: 'client'
+    },
+    devtool: 'source-map',
+    devServer: {
+      static: './dist', // Serve from the dist directory
+    },
+    stats: 'minimal',
+    module: {
+        rules: [
+            {
               test: /\.js$/,
               exclude: /node_modules/,
-              loader: "babel-loader"
-          }
-      ]
-  },
-  plugins: [
-      new HtmlWebpackPlugin({
-          template: "./src/client/views/index.html",
-          filename: "./index.html",
-      })
-  ]
+              use: {
+                loader: "babel-loader",
+                options: {
+                  presets: ['@babel/preset-env'],
+                },
+              },
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./src/client/views/index.html",
+            filename: "./index.html",
+        }),
+        new CleanWebpackPlugin({
+            // Simulate the removal of files
+            dry: true,
+            // Write Logs to Console
+            verbose: true,
+            // Automatically remove all unused webpack assets on rebuild
+            cleanStaleWebpackAssets: true,
+            protectWebpackAssets: false
+        })
+    ]
 }

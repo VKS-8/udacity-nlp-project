c675fdb6f794b1cd4610dd49ce743525ee4b7993
VK Johnson <64655526+VKS-8@users.noreply.github.com>
2023-08-11 12:22:07 -0500

    Add code from another branch

--- fend/client/js/validateInput.mjs
+++ fend/client/js/validateInput.mjs
@@ -1,3 +1,32 @@
 // TODO set up a function that validates the user's input onBlur before submitting
 // Look back at your simple interest calc to see what you put in the html for this
 // type of action
+
+const validateInput = document.querySelector('#evaluationForm').addEventListener('onBlur', () => {
+  const userInput = document.getElementById('article');
+
+  console.log("::: Running checkForArticle :::", userInput);
+
+  if (userInput === null) {
+  // Stop if input is null
+    return false;
+  }
+
+  const articleValue = userInput.value.trim();
+
+  if (articleValue === '') {
+    alert('Please enter a valid article snippet.'); // Alert user to enter an input
+    userInput.focus(); // Set focus back to input field and wait for valid input
+    return false;  // Return false if input is invalid
+  } else {
+    projectData.formText = articleValue; // Store data as an object
+    return true;
+  }
+
+})
+
+
+
+validateInput(); // Do I need to pass in userInput?
+
+export { validateInput };

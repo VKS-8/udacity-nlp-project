70952a47c7199cbd3154eacc76fed787984eb019
VK Johnson <64655526+VKS-8@users.noreply.github.com>
2023-08-11 12:21:41 -0500

    Add comment

--- fend/client/js/inputType.mjs
+++ fend/client/js/inputType.mjs
@@ -1,2 +1,4 @@
 // TODO create a switch statement to determine which input type the user
 // has entered (text, url, doc) and add it to the userInput data object on the server-side
+
+// test app with text input for now

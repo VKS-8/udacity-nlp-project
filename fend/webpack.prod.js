be486fbb5dda36d78e516c6302d93d4dba3f79e7
VK Johnson <64655526+VKS-8@users.noreply.github.com>
2023-08-11 22:48:24 -0500

    Add packages Define in prod.js

--- fend/webpack.prod.js
+++ fend/webpack.prod.js
@@ -2,6 +2,10 @@ const path = require('path')
 const webpack = require('webpack')
 const HtmlWebpackPlugin = require("html-webpack-plugin")
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
+
 
 module.exports = {
     entry: './src/client/index.js',
@@ -9,6 +13,9 @@ module.exports = {
     output: {
       filename: 'bundle.js',
       path: path.resolve(__dirname, 'dist'), // Output directory
+    },
+    optimization: {
+      minimizer: [new TerserPlugin({}), new OptimizeCSSAssetsPlugin({})]
     },
     module: {
         rules: [
@@ -24,24 +31,27 @@ module.exports = {
           },
           {
             test: /\.s[ac]ss$/i,
-            use: [ 'style-loader', 'css-loader', 'sass-loader']
+            use: [ MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
           }
         ]
     },
     plugins: [
-        new HtmlWebpackPlugin({
-            template: "./src/client/views/index.html",
-            filename: "./index.html",
-        }),
-        new CleanWebpackPlugin({
-          // Simulate the removal of files
-          dry: true,
-          // Write Logs to Console
-          verbose: true,
-          // Automatically remove all unused webpack assets on rebuild
-          cleanStaleWebpackAssets: true,
-          protectWebpackAssets: false
+      new HtmlWebpackPlugin({
+          template: "./src/client/views/index.html",
+          filename: "./index.html",
       }),
-        // new WorkboxPlugin.GenerateSW()
+      new CleanWebpackPlugin({
+        // Simulate the removal of files
+        dry: true,
+        // Write Logs to Console
+        verbose: true,
+        // Automatically remove all unused webpack assets on rebuild
+        cleanStaleWebpackAssets: true,
+        protectWebpackAssets: false
+      }),
+      new MiniCssExtractPlugin({
+        filename: "[name].css"
+      }),
+      // new WorkboxPlugin.GenerateSW()
     ]
 }

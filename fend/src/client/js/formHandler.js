8c91590e020e3236a1f2314dae583006bcd3ce1b
VK Johnson <64655526+VKS-8@users.noreply.github.com>
2023-08-12 10:50:02 -0500

    Add require to checkForName Remove Client. from checkForName function call

--- fend/src/client/js/formHandler.js
+++ fend/src/client/js/formHandler.js
@@ -1,11 +1,11 @@
-const checkForName = ('./nameChecker')
+const checkForName = require('./nameChecker');
 
 function handleSubmit(event) {
   event.preventDefault()
 
   // check what text was put into the form field
   let formText = document.getElementById('name').value
-  Client.checkForName(formText)
+  checkForName(formText)
 
   console.log("::: Form Submitted :::")
   fetch('http://localhost:5501/test')

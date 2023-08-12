37b2799af67dbaf36559acf114b93e01a4f55386
VK Johnson <64655526+VKS-8@users.noreply.github.com>
2023-08-11 20:46:51 -0500

    Add validateInput logic

--- fend/src/client/js/nameChecker.js
+++ fend/src/client/js/nameChecker.js
@@ -1,16 +1,19 @@
 function checkForName(inputText) {
   console.log("::: Running checkForName :::", inputText);
-  let names = [
-      "Picard",
-      "Janeway",
-      "Kirk",
-      "Archer",
-      "Georgiou"
-  ]
+  const nameInput = document.getElementById('name');
 
-  if(names.includes(inputText)) {
-      alert("Welcome, Captain!")
+  if (nameInput === null) {
+    // Stop if input is null
+    return;
   }
+
+  const nameValue = nameInput.value.trim();
+
+  if (nameValue === '') {
+    alert('Please enter a valid name.'); // Alert user to enter an input
+    nameInput.focus(); // Set focus back to input field and wait for valid input
+  };
+
 }
 
 export { checkForName }

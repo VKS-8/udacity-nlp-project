b557b758ca29c65ba82e0574458fad8c3143ca53
VK Johnson <64655526+VKS-8@users.noreply.github.com>
2023-08-13 13:02:51 -0500

    Consolidate function imports to Client object Import Client object Move input validation to validateInput.js Add Client.validateInput()

--- fend/src/client/index.js
+++ fend/src/client/index.js
@@ -1,5 +1,4 @@
-import { checkForName } from './js/nameChecker'
-import { handleSubmit } from './js/formHandler'
+import { Client } from './js/client'
 
 import './styles/resets.scss'
 import './styles/base.scss'
@@ -7,80 +6,78 @@ import './styles/footer.scss'
 import './styles/form.scss'
 import './styles/header.scss'
 
-// Create the Client namespace
-const Client = {};
+// // Create the Client namespace
+// const Client = {};
 
-// Attach functions to the Client namespace
-Client.checkForName = checkForName;
-Client.handleSubmit = handleSubmit;
+// // Attach functions to the Client namespace
+// Client.checkForName = checkForName;
+// Client.handleSubmit = handleSubmit;
 
-const form = document.querySelector('form');
+// function validateInput() {
+//   const form = document.querySelector('form');
 
-form.addEventListener('submit', (event) => {
-  event.preventDefault();
+//   form.addEventListener('submit', (event) => {
+//     event.preventDefault();
 
-  // Get input text from form
-  const inputText = document.getElementById('name').value;
+//     // Get input text from form
+//     const inputText = document.getElementById('name').value;
 
-  // Validate user input
-  const isValid = checkForName(inputText);
+//     // Validate user input
+//     const isValid = Client.checkForName(inputText);
 
-  if (isValid) {
-    // Submit form
-    Client.handleSubmit(event);
-  }
-});
+//     if (isValid) {
+//       // Submit form
+//       Client.handleSubmit(event);
+//     }
+//   });
+// }
 
 document.addEventListener('DOMContentLoaded', function() {
 
+  Client.validateInput();
 
-  // Used chatGPT to determine how to set up proper parameters from form to
-  // post to server
-  document.getElementById('evaluationForm').addEventListener('submit', async (e) => {
-    e.preventDefault();
+  // // Used chatGPT to determine how to set up proper parameters from form to
+  // // post to server
+  // document.getElementById('evaluationForm').addEventListener('submit', async (e) => {
+  //   e.preventDefault();
 
-    const formData = new FormData(e.target);
-    const userData = Object.fromEntries(formData.entries());
+  //   const formData = new FormData(e.target);
+  //   const userData = Object.fromEntries(formData.entries());
 
-    const fetchData = async (url = '', data = {}) => {
-      console.log(data);
+  //   const fetchData = async (url = '', data = {}) => {
+  //     console.log(data);
 
-      const response = await fetch('http://localhost:8080/', {
-        method: 'POST',
-        credentials: 'same-origin',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(data),
-      });
+  //     const response = await fetch('http://localhost:8080/', {
+  //       method: 'POST',
+  //       credentials: 'same-origin',
+  //       headers: {
+  //         'Content-Type': 'application/json',
+  //       },
+  //       body: JSON.stringify(data),
+  //     });
 
-      try {
-        if (!response.ok) {
-          throw new Error('Network response was not ok.');
-        }
-        const responseData = await response.json();
-        // console.log(responseData);
-        // return responseData;
+  //     try {
+  //       if (!response.ok) {
+  //         throw new Error('Network response was not ok.');
+  //       }
+  //       const responseData = await response.json();
+  //       // console.log(responseData);
+  //       // return responseData;
 
-        // const {userData, subjectivity, scoreTag, agreement} = responseData;
+  //       // const {userData, subjectivity, scoreTag, agreement} = responseData;
 
-        document.querySelector('#results').innerText = responseData;
+  //       document.querySelector('#results').innerText = responseData;
 
-        // `Subjectivity: ${subjectivity}
-        // Polarity: ${scoreTag}
-        // Text: ${userData}
-        // Agreement: ${agreement}`;
+  //       // `Subjectivity: ${subjectivity}
+  //       // Polarity: ${scoreTag}
+  //       // Text: ${userData}
+  //       // Agreement: ${agreement}`;
 
-      } catch (error) {
-        console.error('Error:', error.message);
-        throw error;
-      }
-    }
-    fetchData('/', userData);
-  });
+  //     } catch (error) {
+  //       console.error('Error:', error.message);
+  //       throw error;
+  //     }
+  //   }
+  //   fetchData('/', userData);
+  // });
 });
-
-// export {
-//   checkForName,
-//   handleSubmit
-// };
